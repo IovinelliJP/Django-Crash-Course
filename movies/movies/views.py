7c97bef6ed57d58c7afa921a24f5a7696bd1c7ae
IovinelliJP <jpiovinelli@outlook.com>
2023-03-22 15:58:41 -0500

    templates and other changes

--- movies/movies/views.py
+++ movies/movies/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
 
 def movies(request):
-    return HttpResponse("Hello there")
+    return render(request, 'movies/movies.html', {'movies': ['movie1', 'movie2']})
 
 def home(request):
     return HttpResponse("Home Page")
