3cdaf70da162f7494957ad0334c68a9498ed28fd
IovinelliJP <jpiovinelli@outlook.com>
2023-03-22 16:59:17 -0500

    pages and template fix

--- movies/movies/views.py
+++ movies/movies/views.py
@@ -2,8 +2,28 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 
+data = {
+    'movies': [
+        {
+            'id': 5,
+            'title': 'Jaws',
+            'Year': 1979
+        }, 
+        {
+            'id': 6,
+            'title': 'Sharknado',
+            'Year': 2015
+        },
+        {
+            'id': 7,
+            'title': 'The Meg',
+            'Year': 2020
+        }
+        ]
+}
+
 def movies(request):
-    return render(request, 'movies/movies.html', {'movies': ['movie1', 'movie2']})
+    return render(request, 'movies/movies.html', data)
 
 def home(request):
     return HttpResponse("Home Page")
